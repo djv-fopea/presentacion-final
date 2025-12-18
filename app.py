ec1b8115663a20971803cd3686e41526e4212fe0
djv-fopea <smantero96@gmail.com>
2025-12-17 23:39:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="FOPEA APP - BOOTCAMP DATA VISUALIZATION 2025",
+    page_title="FOPEA APP - BOOTCAMP DJV2025",
     layout="wide"
 )
 
